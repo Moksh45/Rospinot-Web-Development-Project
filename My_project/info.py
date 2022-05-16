a48bfa43cb874fb3e5cf61e2e799f9a13b7f51c6
Moksh Gupta <mokshgupta567@gmail.com>
2022-05-17 00:04:17 +0530

    adding email verification using django

---
+++ My_project/info.py
@@ -0,0 +1,5 @@
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'demoemailverify@gmail.com'
+EMAIL_HOST_PASSWORD = 'helloworldpassword123'
+EMAIL_PORT = 587
--- first_web/views.py
+++ first_web/views.py
@@ -1,16 +1,15 @@
-from argparse import _MutuallyExclusiveGroup
-import email
-from email import message
-from http.client import HTTPResponse
-from imaplib import _Authenticator
-from multiprocessing import AuthenticationError
-from telnetlib import LOGOUT
-from django.shortcuts import redirect, render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib import messages
-from django.contrib.auth import authenticate, login ,logout
-
+from django.core.mail import EmailMessage, send_mail
+from My_project import settings
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
+from django.utils.encoding import force_bytes, force_text
+from django.contrib.auth import authenticate, login, logout
+from . tokens import generate_token
 
 # Create your views here.
 
@@ -28,21 +27,88 @@ def signup(request):
         pass1 = request.POST['pass1']
         pass2 = request.POST['pass2']
 
+        if User.objects.filter(username=username):
+            message.error(
+                request, "Username already exist! please try other username")
+            return redirect('home')
+
+        if User.objects.filter(email=email):
+            message.error(
+                request, "Email already exist! please try other username")
+            return redirect('home')
+
+        if len(username) > 15:
+            message.error(request, "Username must be under 15 character")
+
+        if pass1 != pass2:
+            message.error(request, "Passwords not Matched")
+
+        if not username.isalnum():
+            message.error(request, "Username must be Alpha-Numeric!")
+            return redirect('home')
+
         myuser = User.objects.create_user(username, email, pass1)
         myuser.first_name = fname
         myuser.last_name = lname
-
+        myuser.is_active = False
         myuser.save()
 
-        messages.success(request, "Your Accout has been Successfully Created")
+        messages.success(
+            request, "Your Accout has been Successfully Created. We have sent you a confirmation email, please confirm your email in order to activate youe account")
+
+        # Email Message
+        subject = "Welcome to Fashion Point - django Login!!"
+        message = "Hello " + myuser.first_name + "!! \n" + \
+            "Welcome to Fashion Point \n thank You for visiting our Website \n We have also sent you a confirmation email, please confirm your email address in order to activate your account \n\n Thanking You"
+        from_email = settings.EMAIL_HOST_USER
+        to_list = [myuser.email]
+        send_mail(subject, message, from_email, to_list, fail_silenty=true)
+
+        # Email address confirmation Email
+
+        current_site = get_current_site(request)
+        email_subject = "Confirm your Email @ GFG - Django Login!!"
+        message2 = render_to_string('email_confirmation.html', {
+
+            'name': myuser.first_name,
+            'domain': current_site.domain,
+            'uid': urlsafe_base64_encode(force_bytes(myuser.pk)),
+            'token': generate_token.make_token(myuser)
+        })
+        email = EmailMessage(
+            email_subject,
+            message2,
+            settings.EMAIL_HOST_USER,
+            [myuser.email],
+        )
+        email.fail_silently = True
+        email.send()
 
         return redirect('signin')
 
     return render(request, "first_web/signup.html")
 
 
+def activate(request, uidb64, token):
+    try:
+        uid = force_text(urlsafe_base64_decode(uidb64))
+        myuser = User.objects.get(pk=uid)
+    except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+        myuser = None
+
+    if myuser is not None and generate_token.check_token(myuser, token):
+        myuser.is_active = True
+        # user.profile.signup_confirmation = True
+        myuser.save()
+        login(request, myuser)
+        messages.success(request, "Your Account has been activated!!")
+        return redirect('signin')
+    else:
+        return render(request, 'activation_failed.html')
+
+
 def signin(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         username = request.POST['username']
         pass1 = request.POST['pass1']
 
@@ -53,7 +119,7 @@ def signin(request):
             fname = user.first_name
             return render(request, "first_web/index.html", {'fname': fname})
         else:
-            message.error(request, "Bad Credentials!")
+            messages.error(request, "Bad Credentials!")
             return redirect('home')
 
 
@@ -63,5 +129,5 @@ def signin(request):
 
 def signout(request):
     logout(request)
-    message.success(request, "Logged out Successfully!")
+    messages.success(request, "Logged Out Successfully!!")
     return redirect('home')
